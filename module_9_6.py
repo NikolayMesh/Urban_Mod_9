a9b99c4e376712bb7edf90429e4c975578599604
NikolayMesh <partizan.91@bk.ru>
2024-11-04 19:18:34 +0300

    add

---
+++ module_9_6.py
@@ -0,0 +1,10 @@
+def all_variants(text):
+    length = len(text)
+    for sub_length in range(1, length + 1):
+        for start in range(length - sub_length + 1):
+            yield text[start:start + sub_length]
+
+if __name__ =='__main__':
+    a = all_variants("abc")
+    for i in a:
+        print(i)
